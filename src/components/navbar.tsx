d4088dbdf2a3e36553deed705746af9a73534d8a
UmmeHabiba1312 <gm3088685@gmail.com>
2024-10-03 21:10:03 +0500

    first commit

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 
 function Navbar() {
   return (
-   <div className='hidden lg:block'>
+    <div className='hidden lg:block'>
         <div className="container">
           <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
 
             <Link className='navbar_link relative' href='#'>Home</Link>
             <Link className='navbar_link relative' href='#'>Categories</Link>
-            <Link className='navbar_link relative' href='#'>Men's</Link>
-            <Link className='navbar_link relative' href='#'>Women's</Link>
-            <Link className='navbar_link relative' href='#'>Jewelary</Link>
+            <Link className='navbar_link relative' href='#'>Men&#39;s</Link> {/* Corrected */}
+            <Link className='navbar_link relative' href='#'>Women&#39;s</Link> {/* Corrected */}
+            <Link className='navbar_link relative' href='#'>Jewelry</Link>
             <Link className='navbar_link relative' href='#'>Perfume</Link>
             <Link className='navbar_link relative' href='#'>Blog</Link>
-            <Link className='navbar_link relative' href='#'>Hote Offers</Link>
+            <Link className='navbar_link relative' href='#'>Hot Offers</Link>
           </div>
         </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
